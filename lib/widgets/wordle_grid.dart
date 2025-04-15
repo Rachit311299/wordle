2f54d14cc8f4f36c8376e0bb9443d427963a7d61
Rachit Deo <55102127+Rachit311299@users.noreply.github.com>
2025-04-15 16:42:27 -0700

    bug fixes

--- lib/widgets/wordle_grid.dart
+++ lib/widgets/wordle_grid.dart
@@ -4,17 +4,18 @@ import 'package:wordle/providers/game_settings_provider.dart';
 import 'package:wordle/providers/game_state_provider.dart';
 import 'package:wordle/widgets/wordle_row.dart';
 
-class WordleGrid extends StatelessWidget {
+class WordleGrid extends ConsumerWidget {
   const WordleGrid({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final attempts = ref.watch(GameSettingsProvider.select((s) => s.attempts));
     return RepaintBoundary(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: List.generate(
-          6, 
-          (i) => WordleRow(rowIndex: i, key: ValueKey('row_$i')), 
+          attempts,
+          (i) => WordleRow(rowIndex: i, key: ValueKey('row_$i')),
         ),
       ),
     );
